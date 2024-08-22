06e01da792fb40a1a69c5479650f6c3da5132dc3
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-08-23 04:06:06 +0500

    Milestone-01

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
-import Image from "next/image";
+"use client"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-slate-950 min-h-screen">
       <h3 className="text-white pt-5 pl-3 font-bold ">Nextjs : Milestone 01</h3>
 
-      <div className="border-red-600 min-h-16 px-16 mt-10 flex justify-center text-white flex-col text-center">
+      <div className="min-h-16 px-16 mt-10 flex justify-center text-white flex-col text-center">
         <h1 className="text-4xl mb-6 font-semibold animate-colorChange hover:scale-110 transition-transform duration-500"> Hello World </h1>
-        <h4>
+        <h5> 
           To learn more about the Next.js application file structure, click the
           button below for details.
-        </h4>
+        </h5>
 
-        <button className="bg-blue-700 border mt-11 text-white py-2 px-4 rounded hover:bg-blue-800 transition-colors duration-300 max-w-xs mx-auto block">
+        <Link
+          href={"https://github.com/Tehreem-Asghar/Milestone-01/blob/main/README.md"}
+        >
+        <button className="bg-blue-700 border mt-11 text-white py-2 px-4 rounded hover:bg-blue-800 transition-colors duration-300 max-w-xs mx-auto block ">
           Learn more
         </button>
+        </Link>
         <h4 className="text-white text-center mt-14">
-          <i>Created by GIAIC senior student: </i> <b>Tehreem Asghar</b>
+          <i>Created by GIAIC senior student:  <b>Tehreem Asghar</b></i>
         </h4>
       </div>
     </div>
