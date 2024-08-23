451e65eba54e477f52e73c699682413c6ce1ed08
Tehreem-Asghar <161032033+Tehreem-Asghar@users.noreply.github.com>
2024-08-23 05:11:07 +0500

    Milestone-01

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 import Link from "next/link";
 
-export default function Home() {
+export default function HelloWorld() {
   return (
     <div className="bg-slate-950 min-h-screen">
-      <h3 className="text-white pt-5 pl-3 font-bold ">Nextjs : Milestone 01</h3>
+      <h1 className="text-white pt-5 pl-3 font-bold text-2xl">Nextjs : Milestone 01</h1>
 
-      <div className="min-h-16 px-16 mt-10 flex justify-center text-white flex-col text-center">
-        <h1 className="text-4xl mb-6 font-semibold animate-colorChange hover:scale-110 transition-transform duration-500"> Hello World </h1>
-        <h5> 
+      <div className="min-h-16 px-16 mt-14 flex justify-center text-white flex-col text-center">
+        <h1 className="text-5xl  mt-5 font-semibold animate-colorChange hover:scale-110 transition-transform duration-500"> Hello World </h1>
+        <h5 className="mt-14"> 
           To learn more about the Next.js application file structure, click the
           button below for details.
         </h5>
@@ -20,7 +20,7 @@ export default function Home() {
           Learn more
         </button>
         </Link>
-        <h4 className="text-white text-center mt-14">
+        <h4 className="text-white text-center mt-20">
           <i>Created by GIAIC senior student:  <b>Tehreem Asghar</b></i>
         </h4>
       </div>
